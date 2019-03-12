a8f6fe48c0935c8689f51100be6614cc35908adb
evildoerDb <179453684@qq.com>
2019-03-13 00:36:04 +0800

    String.subString

--- src/main/java/com/bobo/string/StringUtils.java
+++ src/main/java/com/bobo/string/StringUtils.java
@@ -4,4 +4,25 @@ package com.bobo.string;
  * Created by evildoerdb_ on 2019/3/12
  */
 public class StringUtils {
+
+    /**
+     *  subString from start to
+     * @param str
+     * @param start include index of start
+     * @param end not include index of end
+     * @return
+     */
+    public static String subString(String str, int start, int end){
+        if (start < end){
+            return "";
+        }else {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = start; i < end; i++){
+                sb.append(str.charAt(i));
+            }
+
+            return sb.toString();
+        }
+    }
 }
