d11ee044b110848324cb69cd96a37345d0848cd8
evildoerDb <179453684@qq.com>
2019-03-13 00:24:01 +0800

    init repository

---
+++ src/main/java/com/bobo/collections/CollectionsUtils.java
@@ -0,0 +1,7 @@
+package com.bobo.collections;
+
+/**
+ * Created by evildoerdb_ on 2019/3/12
+ */
+public class CollectionsUtils {
+}
---
+++ src/main/java/com/bobo/string/StringUtils.java
@@ -0,0 +1,7 @@
+package com.bobo.string;
+
+/**
+ * Created by evildoerdb_ on 2019/3/12
+ */
+public class StringUtils {
+}
